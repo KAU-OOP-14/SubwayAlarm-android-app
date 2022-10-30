26b95f0221ac1f53440db71cc9c31cd72b19b916
hhtboy <59467684+hhtboy@users.noreply.github.com>
2022-10-30 13:31:48 +0900

    주석 추가

--- app/src/main/java/com/example/subway_alarm/data/repository/StationRepositoryImpl.kt
+++ app/src/main/java/com/example/subway_alarm/data/repository/StationRepositoryImpl.kt
@@ -1,20 +1,28 @@
 package com.example.subway_alarm.data.repository
 
 import com.example.subway_alarm.data.api.StationApi
+import com.example.subway_alarm.data.api.StationApiStorage
 
 
+/** 즐겨찾기나 알람설정을 해 놓은 Station을 저장하는 저장소입니다.
+ * 지속적으로 api 데이터를 갱신받고 싶은 Station을 저장합니다.
+ */
 class StationRepositoryImpl(
     var stationApi: StationApi
 ): StationRepository {
+    val stations = arrayOfNulls<String>(100) // 이후에 Station class로 변경 예정
 
     override fun refreshStations() {
     }
 
-    override fun getStationArrivals(stationName: String): String {
-        return stationApi.getStationApiData(stationName)
+    override fun addStationArrival(stationName: String) {
+        stationApi.getApiData()
+
     }
 
-    override fun updateStations() {
+    override fun updateThisStation(stationName: String) {
+
+
     }
 
 }
