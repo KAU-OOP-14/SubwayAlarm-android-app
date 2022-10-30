f8bd363d0a60d7ec06d06a3e016742834fef7e38
hhtboy <59467684+hhtboy@users.noreply.github.com>
2022-10-30 13:31:09 +0900

    api 데이터를 저장하는 Storage 구현

--- app/src/main/java/com/example/subway_alarm/data/api/StationApiStorage.kt
+++ app/src/main/java/com/example/subway_alarm/data/api/StationApiStorage.kt
@@ -1,44 +1,77 @@
 package com.example.subway_alarm.data.api
 
-class StationApiStorage: StationApi {
-    /*
-    val key: String = "7a5370504e6868743131324443656265"
-    val type: String = "xml"
-    val service: String = "realtimeStationArrival"
-    val startIndex: String = "0"
-    val endIndex: String = "5"
-    val stationName: String = "서울"
-    val url: String = "http://swopenAPI.seoul.go.kr/api/subway/$key/$type/$service/$startIndex/$endIndex/$stationName"
+import androidx.lifecycle.ViewModel
+import com.example.subway_alarm.models.ViewModelImpl
+import org.w3c.dom.Document
+import org.xml.sax.InputSource
+import java.io.StringReader
+import javax.xml.parsers.DocumentBuilder
+import javax.xml.parsers.DocumentBuilderFactory
 
-    Thread {
-        val urlBuilder = StringBuilder("http://swopenAPI.seoul.go.kr/api/subway/7a5370504e6868743131324443656265/xml/realtimeStationArrival/0/5/서울") /*URL*/
-        val url = URL(urlBuilder.toString())
-        val conn = url.openConnection() as HttpURLConnection
-        conn.requestMethod = "GET"
-        conn.setRequestProperty("Content-type", "application/xml")
-        println("Response code: " + conn.responseCode.toString()) /* 연결 자체에 대한 확인이 필요하므로 추가합니다.*/
+class StationApiStorage() : StationApi {
 
-        // 서비스코드가 정상이면 200~300사이의 숫자가 나옵니다.
-        val rd: BufferedReader = if (conn.responseCode in 200..300) {
-            BufferedReader(InputStreamReader(conn.inputStream))
-        } else {
-            BufferedReader(InputStreamReader(conn.errorStream))
-        }
-        val sb = StringBuilder()
-        var line: String?
-        while (rd.readLine().also { line = it } != null) {
-            sb.append(line)
-        }
-        rd.close()
-        conn.disconnect()
-        println(sb.toString())
-        text += sb.toString()
+    var apiStringData: String? = null
+    val parsedApiData: Array<String?> = arrayOfNulls<String>(8)
 
-    }.start()
+    override fun getApiData(): Array<String?> {
+        return parsedApiData
+    }
 
+    /** 전달받은 station의 이름으로 api 데이터를 요청하고
+     * 파싱되지 않은 String 결과를 반환합니다. */
+    override fun setStringApiData(stringApiData: String){
+        apiStringData = stringApiData
+
+    }
+
+    /** 전달받은 string 타입의 api 데이터를 파싱합니다.
+     * 파싱한 데이터를 Array<String?>으로 반환합니다.
      */
+    override fun setParseApiData(apiStringData: String?) {
+        // xml string 데이터를 document로 변환하고 파싱하기
+        val factory: DocumentBuilderFactory = DocumentBuilderFactory.newInstance()
+        val builder: DocumentBuilder = factory.newDocumentBuilder()
+        val document: Document = builder.parse(InputSource(StringReader(apiStringData)))
 
-    override fun getStationApiData(stationName: String): String {
-        return "this is a mvvm test"
+        // 전달받는 열차 하나의 데이터 리스트
+        // item 값을 증가시켜서 다른 열차의 데이터를 받을 수 있음
+
+        //데이터를 못 받아올 경우의 예외처리
+        try {
+            val lineId: String =
+                document.getElementsByTagName("subwayId").item(0).getChildNodes().item(0).getNodeValue()
+            val trainNo: String =
+                document.getElementsByTagName("btrainNo").item(0).getChildNodes().item(0).getNodeValue()
+            val destDirection: String =
+                document.getElementsByTagName("trainLineNm").item(0).getChildNodes().item(0)
+                    .getNodeValue()
+            val destStationName: String =
+                document.getElementsByTagName("bstatnNm").item(0).getChildNodes().item(0).getNodeValue()
+            val arrivalCode: String =
+                document.getElementsByTagName("arvlCd").item(0).getChildNodes().item(0).getNodeValue()
+            val arrivalTime: String =
+                document.getElementsByTagName("barvlDt").item(0).getChildNodes().item(0).getNodeValue()
+            val innerOuter: String =
+                document.getElementsByTagName("updnLine").item(0).getChildNodes().item(0).getNodeValue()
+            val receivedTime: String =
+                document.getElementsByTagName("recptnDt").item(0).getChildNodes().item(0).getNodeValue()
+
+            parsedApiData[0] = lineId
+            parsedApiData[1] = trainNo
+            parsedApiData[2] = destDirection
+            parsedApiData[3] = destStationName
+            parsedApiData[4] = arrivalCode
+            parsedApiData[5] = arrivalTime
+            parsedApiData[6] = innerOuter
+            parsedApiData[7] = receivedTime
+        }catch(npe: NullPointerException) {
+            println("데이터가 없습니다")
+        }
+
+
+
+
+
     }
 }
+
