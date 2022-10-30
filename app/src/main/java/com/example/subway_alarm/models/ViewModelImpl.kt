b101ea3129decbaf064e53b1bc8ed14b18a28a04
hhtboy <59467684+hhtboy@users.noreply.github.com>
2022-10-30 13:33:44 +0900

    view model에 api관련 live data 조작 구현

--- app/src/main/java/com/example/subway_alarm/models/ViewModelImpl.kt
+++ app/src/main/java/com/example/subway_alarm/models/ViewModelImpl.kt
@@ -3,27 +3,53 @@ package com.example.subway_alarm.models
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.example.subway_alarm.data.api.ApiThread
 import com.example.subway_alarm.data.api.StationApi
-import com.example.subway_alarm.data.api.StationApiStorage
 import com.example.subway_alarm.data.repository.StationRepository
+import org.koin.core.component.KoinComponent
+import org.koin.core.component.inject
 
 class ViewModelImpl(
-    var stationRepository: StationRepository
-): ViewModel() {
-    private val _data = MutableLiveData<String>()
-    val data: LiveData<String>
+    val stationRepository: StationRepository,
+    val stationApiStorage: StationApi,
+): ViewModel(), KoinComponent {
+    private val _data = MutableLiveData<Array<String?>>()
+
+    // Getter
+    val data: LiveData<Array<String?>>
         get() = _data
 
     //초기값
     init {
         println("ViewModelImpl - 생성자 호출")
-        _data.value = "초기값"
+        _data.value = arrayOfNulls<String>(8)
     }
 
-    /* view에서 준 input값으로 데이터를 변경
-    * Repository에 있는 데이터를 불러옵니다 */
-    fun updateData(stationName: String) {
-        _data.value = stationRepository.getStationArrivals(stationName)
+    /**
+     * 스레드가 끝난 후, LiveData를 업데이트 합니다.
+     * ApiThread에서 자동 호출 됩니다.
+     */
+    fun updateData() {
+        val testArr = stationApiStorage.getApiData()
+        _data.value = testArr
+    }
+
+    /**
+     * 새로운 api를 요청합니다.
+     * StationApiStorage 에 저장됩니다.
+     */
+    fun requestApiData(stationName: String) {
+        //새로운 스레드르 의존성 주입으로 생성합니다.
+        val thread: ApiThread by inject()
+        thread.setStationName(stationName)
+        thread.start()
+        thread.join()
+        updateData()
+    }
+
+    override fun onCleared() {
+        super.onCleared()
+        println("리소스 정리")
     }
 
 }
