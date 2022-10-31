9b87db1cef86427704d7032c01e2d3d1e17c0375
Serenata2 <astg98@gmail.com>
2022-10-31 23:56:50 +0900

    Station 클래스의 set함수 구현, Line 클래스의 endPointList 프로퍼티 간단하게 선언

--- app/src/main/java/com/example/subway_alarm/models/Station.kt
+++ app/src/main/java/com/example/subway_alarm/models/Station.kt
@@ -9,9 +9,137 @@ class Station(
     var rightStation: Station? = null
     var left2Station: Station? = null
     var right2Station: Station? = null
+    var endPoint = arrayOf("", "")
+
+    // Station 객체의 left,right station 및 endPoint 초기화해주는 함수
+    fun set(index: Int, size: Int){
+        // 일반적인 left, right station 초기화
+        if(index == 0)
+            rightStation = super.stations[1]
+        else if (index >= (size-1))
+            leftStation = super.stations[size-2]
+        else{
+            leftStation = super.stations[index-1]
+            rightStation = super.stations[index+1]
+        }
+
+        if(super.lineId == 1){ // 1호선
+            if(index <= 41){
+                endPoint[0] = super.endPointList[0]
+                endPoint[1] = super.endPointList[1]
+                if(index == 41)
+                    right2Station = super.stations[62] // 가산디지털단지
+            }
+            else if(index <= 61){
+                endPoint[0] = super.endPointList[0]
+                endPoint[1] = super.endPointList[2]
+                if(index == 61)
+                    rightStation = null // 인천(종착)
+            }
+            else{
+                endPoint[0] = super.endPointList[3]
+                endPoint[1] = super.endPointList[4]
+            }
+        }
+
+        else if(super.lineId == 2){ // 2호선
+            if(index == 10){        // 신도림역
+                endPoint[0] = super.endPointList[2]
+                endPoint[1] = super.endPointList[1]
+                left2Station = super.stations[50] // 도림천 역
+            }
+            else if(index == 33){   // 성수역
+                endPoint[0] = super.endPointList[0]
+                endPoint[1] = super.endPointList[5]
+                right2Station = super.stations[43] // 용답 역
+            }
+            else if(index >= 47){
+                endPoint[0] = super.endPointList[3]
+                endPoint[1] = super.endPointList[4]
+                if(index == 50) // 도림천 역
+                    rightStation = super.stations[10] // 신도림역
+                else if(index == 47) // 까치산(종착)
+                    leftStation = null
+            }
+            else if(index >= 43){
+                endPoint[0] = super.endPointList[6]
+                endPoint[1] = super.endPointList[7]
+                if(index == 43) // 용답 역
+                    leftStation = super.stations[33] // 성수역
+                else if (index == 46) // 신설동(종착)
+                    rightStation = null
+            }
+            else{
+                endPoint[0] = super.endPointList[0]
+                endPoint[1] = super.endPointList[1]
+                if(index == 0)          // 순환 만들어주기
+                    leftStation = super.stations[42]
+                else if(index == 42)    // 순환 만들어주기
+                    rightStation = super.stations[0]
+            }
+        }
+
+        else if(super.lineId == 5){ // 5호선
+            endPoint[0] = super.endPointList[0]
+            if(index <= 38){
+                endPoint[1] = super.endPointList[1]
+                if(index == 38) // 강동역
+                    right2Station = super.stations[46]  // 길동역
+            }
+            else if(index <= 45){
+                endPoint[1] = super.endPointList[2]
+            }
+            else{
+                endPoint[1] = super.endPointList[3]
+            }
+        }
+
+        else if(super.lineId == 6){ // 6호선
+            if(index <= 4){
+                endPoint[0] = super.endPointList[0]
+                endPoint[1] = super.endPointList[1]
+                leftStation = null // 단선
+            }
+            else{
+                endPoint[0] = super.endPointList[2]
+                endPoint[1] = super.endPointList[1]
+                if (index == 5) // 응암
+                    leftStation = super.stations[0] // 역촌
+            }
+        }
+
+        else if(super.lineId == 10){    // 경의 중앙선
+            endPoint[0] = super.endPointList[0]
+            if(index <= 19){
+                endPoint[1] = super.endPointList[1]
+                if(index == 19) // 가좌역인 경우
+                    right2Station = super.stations[53] // 신촌
+            }
+            else if(index <= 52){
+                endPoint[1] = super.endPointList[2]
+            }
+            else{
+                endPoint[1] = super.endPointList[3]
+            }
+        }
+
+        else{ // 나머지 경우
+            endPoint[0] = super.endPointList[0]
+            endPoint[1] = super.endPointList[1]
+        }
+
+    }
 
     fun getStnName(): String {
         return stationName
+    }
+
+    fun getStnId(): Int{
+        return id
+    }
+
+    fun getIsFavorited(): Boolean{
+        return isFavorited
     }
 
 
