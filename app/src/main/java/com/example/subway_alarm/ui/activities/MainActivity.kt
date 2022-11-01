d6a5e041d6d1633d77ef6407aba2545995bda863
hhtboy <59467684+hhtboy@users.noreply.github.com>
2022-11-01 22:20:17 +0900

    불필요 코드 정리

--- app/src/main/java/com/example/subway_alarm/ui/activities/MainActivity.kt
+++ app/src/main/java/com/example/subway_alarm/ui/activities/MainActivity.kt
@@ -1,14 +1,11 @@
 package com.example.subway_alarm.ui.activities
 
 import android.os.Bundle
-import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
-import com.example.subway_alarm.data.api.ApiThread
 import com.example.subway_alarm.databinding.ActivityMainBinding
 import com.example.subway_alarm.models.ViewModelImpl
-import org.koin.android.ext.android.inject
 import org.koin.android.viewmodel.ext.android.viewModel
 
 
@@ -18,9 +15,8 @@ class MainActivity : AppCompatActivity() {
     view model DI(의존성 주입)
     view는 모든 로직 처리를 view model에게 접근해서 합니다.
      */
-    val mviewModel: ViewModelImpl by viewModel()
+    val viewModel: ViewModelImpl by viewModel()
     lateinit var binding: ActivityMainBinding
-    var flag: Boolean = false
 
     /** fragment를 열어주는 함수, 추후 리펙토링 예정 */
     private fun replaceSubwayDataFragment(fragment: Fragment) {
@@ -37,8 +33,10 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+
+
         /* View Model과 View 연결 */
-        mviewModel.data.observe(this, Observer {
+        viewModel.data.observe(this, Observer {
             println("main activity에서 view model의 data 변경 : $it")
             var text: String = ""
             for(element in it) {
@@ -51,10 +49,11 @@ class MainActivity : AppCompatActivity() {
         /* 이런식으로 viewModel을 통해 input값을 알려줍니다
          모든 데이터 처리는 viewModel이 합니다 */
         binding.hwajeon.setOnClickListener {
-            //replaceSubwayDataFragment(SubwayDataFragment.newInstance(getModelData()))
-            mviewModel.requestApiData("홍대입구")
-
+            //입력한 역의 api 요청
+            viewModel.requestApiData("홍대입구")
         }
     }
+
+
 
 }
