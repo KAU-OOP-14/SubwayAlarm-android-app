94ab25101c44395033ebef22e0d002495e96ba84
hhtboy <59467684+hhtboy@users.noreply.github.com>
2022-11-01 22:19:32 +0900

    엑셀 관련 스레드 추가

---
+++ app/src/main/java/com/example/subway_alarm/data/db/ExcelThread.kt
@@ -0,0 +1,43 @@
+package com.example.subway_alarm.data.db
+
+import com.example.subway_alarm.SubwayAlarmApp
+import org.apache.poi.xssf.usermodel.XSSFCell
+import org.apache.poi.xssf.usermodel.XSSFRow
+import org.apache.poi.xssf.usermodel.XSSFWorkbook
+
+class ExcelThread(): Thread() {
+    private val db: MutableList<Array<String>> = mutableListOf()
+
+    override fun run() {
+        try {
+            val context = SubwayAlarmApp.ApplicationContext()
+            val myFileSystem = context.resources.assets.open("stationdata.xlsx")
+            val myWorkBook = XSSFWorkbook(myFileSystem)
+            val sheet = myWorkBook.getSheetAt(0)
+
+            val rowIterator = sheet.rowIterator()
+            while(rowIterator.hasNext()) {
+                val myRow = rowIterator.next() as XSSFRow
+                val colIterator = myRow.cellIterator()
+
+                val item = arrayOf<String>("","","")
+                var colCount = 0
+                while(colIterator.hasNext()) {
+                    val myCell = colIterator.next() as XSSFCell
+                    item[colCount] = myCell.toString()
+                    colCount++
+                }
+                db.add(item)
+                println("지하철 데이터 : ${item[0]} / ${item[1]} / ${item[2]}")
+            }
+        } catch (e: Exception) {
+            println("엑셀 파일을 읽지 못했습니다.")
+        }
+
+    }
+
+    fun getdb(): MutableList<Array<String>> {
+        return db
+    }
+
+}
