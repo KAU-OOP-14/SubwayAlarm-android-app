7bd2f18802f3f76dc302f4d5f350e6c858ab9675
Serenata2 <astg98@gmail.com>
2022-11-04 01:40:33 +0900

    지하철 객체의 edgePoint를 설정해 주는 함수들 구현했습니다. 그리고 station 엑셀 파일을 수정했습니다.

--- app/src/main/java/com/example/subway_alarm/models/Subway.kt
+++ app/src/main/java/com/example/subway_alarm/models/Subway.kt
@@ -2,7 +2,24 @@ package com.example.subway_alarm.models
 
 object Subway {
     val lines: MutableList<Line> = mutableListOf()
-
+    val edgeList = arrayOf(
+        listOf("소요산", "인천/신창", "인천", "광운대", "신창"),
+        listOf("내선(시계)", "외선(반시계)", "내선/까치산", "까치산", "신도림", "외선/신설동", "성수", "신설동"),
+        listOf("대화", "오금"),
+        listOf("진접", "당고개"),
+        listOf("방화", "하남검단산/마천", "마천", "하남검단산"),
+        listOf("단선", "신내", "응암순환"),
+        listOf("장암", "석남"),
+        listOf("암사", "모란"),
+        listOf("중앙보훈병원", "개화"),
+        listOf("문산", "서울역/지평", "지평", "서울역"),
+        listOf("서울역", "인천공항2터미널"),
+        listOf("청량리", "춘천"),
+        listOf("왕십리/청량리", "인천"),
+        listOf("신사", "광교"),
+        listOf("인천공항1터미널", "용유"),
+        listOf("신설동", "북한산우이")
+    )
     init {
         println("Subway 생성")
     }
@@ -31,7 +48,9 @@ object Subway {
         return null
     }
 
-    fun addLines(line: Line) {
+    fun addLines(line: Line){
+        line.endPointList.addAll(edgeList[line.lineId - 1])
+        line.setStations()
         lines.add(line)
     }
 }
