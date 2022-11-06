76dfb15100c0003e79ac201418af40219fef81d7
hhtboy <59467684+hhtboy@users.noreply.github.com>
2022-11-07 00:10:42 +0900

    주석 수정 및 import 수정

--- app/src/main/java/com/example/subway_alarm/viewModel/ViewModelImpl.kt
+++ app/src/main/java/com/example/subway_alarm/viewModel/ViewModelImpl.kt
@@ -6,20 +6,15 @@ import com.example.subway_alarm.extensions.NonNullMutableLiveData
 import com.example.subway_alarm.model.Station
 import com.example.subway_alarm.model.api.dataModel.ApiModel
 import com.example.subway_alarm.model.api.service.NetworkManager
-import com.example.subway_alarm.model.api.service.NetworkService
 import com.example.subway_alarm.model.repository.StationRepository
 import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
 import io.reactivex.rxjava3.kotlin.addTo
 import io.reactivex.rxjava3.schedulers.Schedulers
 
-import retrofit2.Retrofit
-import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
-import retrofit2.converter.gson.GsonConverterFactory
-
 class ViewModelImpl(
     private val stationRepository: StationRepository,
 ) : ViewModel() {
-    enum class direction { LEFT, RIGHT }
+    enum class Direction { LEFT, RIGHT }
 
     //api model list
     private val _apis = NonNullMutableLiveData<List<ApiModel>>(listOf(ApiModel()))
@@ -58,29 +53,18 @@ class ViewModelImpl(
             _curStation.value = stationRepository.searchResultList[0]
             stationRepository.curStation = stationRepository.searchResultList[0]
         } else return
-
-        /*stationRepository.curStation = Subway.let {
-            val list = it.searchStations(stationName)
-            if (list != null) {
-                println("새로운 curruent station set : ${list[0].stationName}")
-                _curStation.value = list[0]
-                list[0]
-            } else return
-        }*/
     }
 
-    /**
-     * 근접한 station으로 방향을 지정해 이동합니다.
-     */
-    fun gotoStation(direction: Enum<direction>, i: Int = -1) {
+    /** 근접한 station으로 방향을 지정해 이동합니다. */
+    fun gotoStation(direction: Enum<Direction>, i: Int = -1) {
         val node1: Station?
         val node2: Station?
         when (direction) {
-            ViewModelImpl.direction.LEFT -> {
+            Direction.LEFT -> {
                 node1 = stationRepository.curStation.leftStation
                 node2 = stationRepository.curStation.left2Station
             }
-            ViewModelImpl.direction.RIGHT -> {
+            Direction.RIGHT -> {
                 node1 = stationRepository.curStation.rightStation
                 node2 = stationRepository.curStation.right2Station
             }
