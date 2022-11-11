1cd5cbca7fd23813d28c379e88e32f0308dbcd0d
Serenata2 <astg98@gmail.com>
2022-11-12 01:36:06 +0900

    search를 위한 어뎁터와 xml파일 하나를 만들었고, StationRepository를 수정 중에 있습니다.

---
+++ app/src/main/java/com/example/subway_alarm/ui/adapter/SearchedListAdapter.kt
@@ -0,0 +1,28 @@
+package com.example.subway_alarm.ui.adapter
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.subway_alarm.databinding.SearchedListStationsBinding
+import com.example.subway_alarm.model.Station
+
+class SearchedListAdapter
+    : RecyclerView.Adapter<SearchedListAdapter.Holder>(){
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): Holder {
+        TODO("Not yet implemented")
+
+    }
+
+    override fun onBindViewHolder(holder: Holder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+
+    inner class Holder(view: View): RecyclerView.ViewHolder(view){
+    }
+}
