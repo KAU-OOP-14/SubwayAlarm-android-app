6c3fed044ab8954b922508aea5dd9e794cb09ec7
hhtboy <59467684+hhtboy@users.noreply.github.com>
2022-11-17 02:57:33 +0900

    adapter를 재생성하던 문제 해결

--- app/src/main/java/com/example/subway_alarm/SearchFragment.kt
+++ app/src/main/java/com/example/subway_alarm/SearchFragment.kt
@@ -6,8 +6,10 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.SearchView
+import androidx.lifecycle.Observer
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.subway_alarm.databinding.FragmentSearchBinding
 import com.example.subway_alarm.model.Station
 import com.example.subway_alarm.model.Subway
@@ -16,10 +18,10 @@ import com.example.subway_alarm.viewModel.ViewModelImpl
 import com.example.subway_alarm.viewModel.listener.OnSearchResultClick
 import org.koin.android.viewmodel.ext.android.viewModel
 
-class SearchFragment : Fragment(),OnSearchResultClick {
+class SearchFragment : Fragment(), OnSearchResultClick {
     var binding: FragmentSearchBinding? = null
     val viewModel by viewModel<ViewModelImpl>()
-    private val stationList: MutableList<Station> = mutableListOf()
+    private var stationList: MutableList<Station> = mutableListOf()
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -35,16 +37,17 @@ class SearchFragment : Fragment(),OnSearchResultClick {
         // Inflate the layout for this fragment
         binding = FragmentSearchBinding.inflate(inflater, container, false)
 
-        for(line in Subway.lines){
+        for (line in Subway.lines) {
             stationList.addAll(line.stations)
         }
 
-        // 리사이컬뷰를 adapter와 연결
+
+        // 리사이컬뷰를 adapter와 연결, adapter는 한번만 생성합니다.
         binding?.recStations?.layoutManager = LinearLayoutManager(context)
         binding?.recStations?.adapter = SearchedListAdapter(stationList, this)
 
         val searchViewTextListener: SearchView.OnQueryTextListener =
-            object : SearchView.OnQueryTextListener{
+            object : SearchView.OnQueryTextListener {
                 // 검색버튼 입력시 호출, 검색버튼이 없으므로 사용하지 않음
                 override fun onQueryTextSubmit(s: String?): Boolean {
                     return false
@@ -52,8 +55,13 @@ class SearchFragment : Fragment(),OnSearchResultClick {
 
                 //텍스트 입력/수정시에 호출
                 override fun onQueryTextChange(s: String?): Boolean {
-                    s?.let {
-                        binding?.recStations?.adapter = SearchedListAdapter(viewModel.onSearchTextChanged(s), this@SearchFragment)
+                    s?.apply {
+                        //새로운 어뎁터를 만들지 않고, 데이터만 변경한 후 변경사항을 어뎁터에게 notify
+                        stationList.clear()
+                        for(station in viewModel.onSearchTextChanged(s)){
+                            stationList.add(station)
+                        }
+                        binding?.recStations?.adapter?.notifyDataSetChanged()
                     }
                     return false
                 }
@@ -61,6 +69,8 @@ class SearchFragment : Fragment(),OnSearchResultClick {
             }
         // SearchView에 OnQueryTextLitener 부착
         binding?.searchStation?.setOnQueryTextListener(searchViewTextListener)
+
+
 
         return binding?.root
     }
