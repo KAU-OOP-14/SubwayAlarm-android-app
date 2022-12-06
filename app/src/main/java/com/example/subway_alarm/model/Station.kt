a4e78369de95ff2999bdfcaeaa1bdb9dcb4c78a6
Serenata2 <astg98@gmail.com>
2022-12-06 11:21:44 +0900

    model 부분에서 stationId를 분석할 때, STATION_ID_UINT 상수를 만들어서 쓰도록 했고, MutableList에서 ArrayList로 바꾸고자 했습니다.

--- app/src/main/java/com/example/subway_alarm/model/Station.kt
+++ app/src/main/java/com/example/subway_alarm/model/Station.kt
@@ -6,7 +6,7 @@ data class Station(
     /** 역 id */
     val id: Int,
     /** 호선 정보 리스트 */
-    val lineList: MutableList<Int>
+    val lineList: ArrayList<Int>
 ) {
     var isFavorited = false // 즐겨찾기 포함 여부
     var leftStation: Station? = null
